831747207a9ce2d220e6ff5a7703823c374f4601
Michael Kilcorse <kilcorse.michael92@gmail.com>
2019-02-03 22:36:39 -0500

    Update script.js
    cleaned up extra white space and return keys

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -1,9 +1,4 @@
-//Global scope variables
-
-
-
 //function to generate random number and return a quote from the quote[]
-
 function getRandomQuote(){
 var upper = quotes.length;
 var randNum = Math.floor(Math.random()*upper);
@@ -14,6 +9,7 @@ var randNum = Math.floor(Math.random()*upper);
 function getRandomColor(){
   return  Math.floor(Math.random()*256);
 }
+
 //function to change background color
 function getRandomBackground(){
   var background = 'rgb(';
@@ -23,31 +19,22 @@ function getRandomBackground(){
   return background;
 }
 
-
-
-
-
-
 //basic print function to keep printQuote function readable
-
 function print(message){
   var quoteBoxDiv= document.getElementById('quote-box');
   quoteBoxDiv.innerHTML = message;
 }
 
 //function to print the quote to the screen need to clear when event listener is
-
 function printQuote(){
-
-    var randQuote = getRandomQuote();
-    var message = ' ';
-    var randBackground = getRandomBackground();
+  var randQuote = getRandomQuote();
+  var message = ' ';
+  var randBackground = getRandomBackground();
   document.body.style.backgroundColor = randBackground;
   document.getElementById('loadQuote').style.backgroundColor = randBackground;
   message += "<p class='quote'>" +randQuote.quote + "</p>";
   message += "<p class='source'>" +randQuote.source + "<span class='citation'>" +randQuote.citation+ "</span></p>";
   return print(message)
-
 }
 
 //button
